99869cec5ab859a6d90af91b37d391624d824e26
Anderson Saunders <andsbf@gmail.com>
2015-04-21 17:45:13 -0700

    api fix for ember

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes(*Category.attribute_names.map(&:to_sym))
   
-  has_many :courses, embed: :ids, key: "courses", include: true
+  has_many :courses # , embed: :ids, key: "courses", include: true
 end
