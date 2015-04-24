fe0054adf84a414fc9c8f35aebbb5418c2a17c7c
Anderson Saunders <andsbf@gmail.com>
2015-04-24 16:01:43 -0700

    typo correction

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -2,7 +2,7 @@ class CourseSerializer < ActiveModel::Serializer
   attributes(*Course.attribute_names.map(&:to_sym), :category)
 
   has_many :events, embed: :ids, key: "events"#, include: true
-  has_many :registraions, embed: :ids, key: "registrations"#, include: true
+  has_many :registrations, embed: :ids, key: "registrations"#, include: true
 
   def category
     self.category_id
