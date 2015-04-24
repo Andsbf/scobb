1458693da64af150e237838948fb15f72eef1ad6
Anderson Saunders <andsbf@gmail.com>
2015-04-24 13:33:30 -0700

    put has many registration in dependant

--- app/serializers/dependant_serializer.rb
+++ app/serializers/dependant_serializer.rb
@@ -1,4 +1,5 @@
 class DependantSerializer < ActiveModel::Serializer
   attributes(*Dependant.attribute_names.map(&:to_sym))
 
+  has_many :registrations, embed: :ids, key: "registrations"#, include: true
 end
--- app/serializers/registration_serializer.rb
+++ app/serializers/registration_serializer.rb
@@ -1,4 +1,6 @@
 class RegistrationSerializer < ActiveModel::Serializer
   attributes(*Registration.attribute_names.map(&:to_sym))
 
+   # has_one :dependant, embed: :ids, key: "dependant"#, include: true
+   # has_one :payment, embed: :ids, key: "payment"#, include: true
 end
